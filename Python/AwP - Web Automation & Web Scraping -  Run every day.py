f4a16ce82c0e1b15909d8ddbc54efeb2f66d04df
leecharlenej <lee.charlenej@gmail.com>
2024-08-04 17:33:34 +0800

    Python: Automate with Python - Completed web scraping.

---
+++ Python/AwP - Web Automation & Web Scraping -  Run every day.py
@@ -0,0 +1,66 @@
+# To play with XPaths: https://scrapinghub.github.io/xpath-playground/
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import pandas as pd
+
+# Headless mode
+from selenium.webdriver.chrome.options import Options
+
+# Run every day
+from datetime import datetime
+import os
+import sys
+
+website = "https://www.thesun.co.uk/sport/football"
+path = "C:\\Users\\leech\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
+
+# Headless mode
+options = Options()
+# options.headless = True # Not working
+options.add_argument("--headless=new")
+
+# Run every day
+application_path = os.path.dirname(sys.executable)
+now = datetime.now()
+month_day_year = now.strftime("%m%d%Y")
+
+service = Service(executable_path = path)
+# driver = webdriver.Chrome(service = service)
+driver = webdriver.Chrome(service = service, options = options)
+driver.get(website)
+
+# find_elements = Find all elements and return a list.
+containers = driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
+
+titles, subtitles, links = [], [], []
+
+for container in containers:
+
+    try:
+        title = container.find_element(by="xpath", value='./a/span').text
+        subtitle = container.find_element(by="xpath", value='./a/h3').text
+        link = container.find_element(by="xpath", value='./a').get_attribute('href')
+
+        titles.append(title)
+        subtitles.append(subtitle)
+        links.append(link)
+
+    except Exception as e:
+        continue
+
+
+my_dict = {'title': titles,
+           'subtitle': subtitles,
+           'link': links}
+
+df_headlines = pd.DataFrame(my_dict)
+# df_headlines.to_csv('headlines.csv')
+# df_headlines.to_csv(f'{application_path}\headlines-{month_day_year}.csv')
+
+file_name = f'headlines-{month_day_year}.csv'
+final_path = os.path.join('C:\\Users\\leech\\Downloads\\00. School\\Personal\\learning-stuff\Python', file_name) # To ensure correct slashes are correct.
+print(str(final_path))
+df_headlines.to_csv(final_path)
+
+driver.quit()
